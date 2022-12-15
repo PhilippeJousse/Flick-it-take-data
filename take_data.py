a7e66852d6aab30e0e275b3eb7fd9b5b71b11e9f
Philippe <Philicreep2001@gmail.com>
2022-12-15 09:17:17 +0100

    remove key

--- take_data.py
+++ take_data.py
@@ -6,18 +6,11 @@ from PIL import Image
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "google_key.json"
 
-firebaseConfig = {
-  "apiKey": "AIzaSyBnz6wws3EjTRnFOG7NvefKSr9CsaOlcxY",
-  "authDomain": "flick-it-users-storage.firebaseapp.com",
-  "databaseURL": "https://flick-it-users-storage-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "flick-it-users-storage",
-  "storageBucket": "flick-it-users-storage.appspot.com",
-  "messagingSenderId": "1046722019798",
-  "appId": "1:1046722019798:web:905b021820e1922f95a477",
-  "measurementId": "G-J3T9K8WPV2",
-  "serviceAccount": "serviceAccountCredentials.json"
-}
-firebase = pyrebase.initialize_app(firebaseConfig)
+with open('firebaseConfig.json') as f:
+    firebaseConfig = f.read()
+    firebaseConfigContent = json.loads(firebaseConfig)
+
+firebase = pyrebase.initialize_app(firebaseConfigContent)
 
 db = firebase.database()
 
